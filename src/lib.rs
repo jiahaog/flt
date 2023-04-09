1fbd00e21384d5bbdd49e150a14129c36f8ac903
Jia Hao Goh <jiahaog@users.noreply.github.com>
2023-04-09 13:00:40 +0800

    Move main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,201 +1,287 @@
-//! Implementation of [crate::window::Window], rendering to the terminal output.
-//!
-//! This should be the only file in this crate which depends on [crossterm].
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-use std::io::stdout;
-use std::io::Stdout;
-use std::io::Write;
-use std::iter::zip;
+use clap::Parser;
+use crossterm::event::read;
+use crossterm::event::MouseButton;
+use crossterm::event::MouseEvent;
+use std::ffi::CStr;
+use std::time::Instant;
+use std::{ffi::CString, slice, time::Duration};
+use terminal::{Pixel, Terminal};
 
-use crossterm::cursor::{Hide, MoveTo, Show};
-use crossterm::event::DisableMouseCapture;
-use crossterm::event::EnableMouseCapture;
-use crossterm::style::{Color, PrintStyledContent, Stylize};
-use crossterm::terminal::{
-    disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen,
-};
-use crossterm::ErrorKind;
-use crossterm::ExecutableCommand;
-use crossterm::QueueableCommand;
+mod terminal;
 
-pub struct TerminalWindow {
-    stdout: Stdout,
-    lines: Vec<Vec<(Pixel, char)>>,
-    // TODO: Support changing this at runtime.
-    width: usize,
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+const FPS: usize = 60;
+
+extern "C" fn software_surface_present_callback(
+    user_data: *mut std::os::raw::c_void,
+    allocation: *const std::os::raw::c_void,
+    row_bytes: usize,
     height: usize,
+) -> bool {
+    let allocation: &[u8] =
+        unsafe { slice::from_raw_parts(allocation as *const u8, row_bytes * height) };
+
+    let user_data: &mut UserData = unsafe { std::mem::transmute(user_data) };
+    let terminal_window = &mut user_data.terminal;
+
+    // In allocation, each group of 4 bits represents a pixel. In order, each of
+    // the 4 bits will be [b, g, r, a].
+    let buf = allocation
+        .chunks(4)
+        .into_iter()
+        .map(|c| {
+            let b = c[0];
+            let g = c[1];
+            let r = c[2];
+            let a = c[3];
+
+            Pixel { r, g, b, a }
+        })
+        .collect::<Vec<Pixel>>();
+
+    terminal_window.update(&buf).unwrap();
+
+    return true;
 }
 
-impl TerminalWindow {
-    pub fn new(width: usize, height: usize) -> Self {
-        let mut stdout = stdout();
+extern "C" fn log_message_callback(
+    tag: *const ::std::os::raw::c_char,
+    message: *const ::std::os::raw::c_char,
+    _user_data: *mut ::std::os::raw::c_void,
+) {
+    // TODO: Print to the main terminal.
+    let tag = to_string(tag);
+    let message = to_string(message);
+    println!("{tag}: {message}");
+}
 
-        // This causes the terminal to be output on an alternate buffer.
-        stdout.execute(EnterAlternateScreen).unwrap();
-        stdout.execute(Hide).unwrap();
+fn to_string(c_str: *const std::os::raw::c_char) -> String {
+    let message = unsafe { CStr::from_ptr(c_str) };
+    let message = message.to_owned();
 
-        enable_raw_mode().unwrap();
-        stdout.execute(EnableMouseCapture).unwrap();
+    message.to_str().unwrap().to_string()
+}
 
-        Self {
-            stdout,
-            lines: vec![vec![]; height / 2],
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    assets_dir: String,
+
+    #[arg(long)]
+    icu_data_path: String,
+}
+
+impl From<Args> for FlutterProjectArgs {
+    fn from(args: Args) -> Self {
+        let assets_path = args.assets_dir;
+        let icu_data_path = args.icu_data_path;
+
+        let arguments = vec!["arg".to_string()];
+
+        FlutterProjectArgs {
+            struct_size: std::mem::size_of::<FlutterProjectArgs>(),
+            assets_path: CString::new(assets_path).unwrap().into_raw(),
+            main_path__unused__: std::ptr::null(),
+            packages_path__unused__: std::ptr::null(),
+            icu_data_path: CString::new(icu_data_path).unwrap().into_raw(),
+            command_line_argc: arguments.len() as i32,
+            command_line_argv: std::ptr::null(),
+            platform_message_callback: None,
+            vm_snapshot_data: std::ptr::null(),
+            vm_snapshot_data_size: 0,
+            vm_snapshot_instructions: std::ptr::null(),
+            vm_snapshot_instructions_size: 0,
+            isolate_snapshot_data: std::ptr::null(),
+            isolate_snapshot_data_size: 0,
+            isolate_snapshot_instructions: std::ptr::null(),
+            isolate_snapshot_instructions_size: 0,
+            root_isolate_create_callback: None,
+            update_semantics_node_callback: None,
+            update_semantics_custom_action_callback: None,
+            persistent_cache_path: std::ptr::null(),
+            is_persistent_cache_read_only: false,
+            vsync_callback: None,
+            custom_dart_entrypoint: std::ptr::null(),
+            custom_task_runners: std::ptr::null(),
+            shutdown_dart_vm_when_done: true,
+            compositor: std::ptr::null(),
+            dart_old_gen_heap_size: 0,
+            aot_data: std::ptr::null_mut(),
+            compute_platform_resolved_locale_callback: None,
+            dart_entrypoint_argc: 0,
+            dart_entrypoint_argv: std::ptr::null(),
+            log_message_callback: Some(log_message_callback),
+            log_tag: std::ptr::null(),
+            on_pre_engine_restart_callback: None,
+            update_semantics_callback: None,
+        }
+    }
+}
+
+pub fn main() {
+    unsafe {
+        let args = Args::parse();
+        let renderer_config = FlutterRendererConfig {
+            type_: FlutterRendererType_kSoftware,
+            __bindgen_anon_1: FlutterRendererConfig__bindgen_ty_1 {
+                software: FlutterSoftwareRendererConfig {
+                    struct_size: std::mem::size_of::<FlutterSoftwareRendererConfig>(),
+                    surface_present_callback: Some(software_surface_present_callback),
+                },
+            },
+        };
+
+        let (width, height) = crossterm::terminal::size().unwrap();
+        let (width, height) = (width as usize, height as usize);
+        // The terminal renderer merges two pixels (top and bottom) into one.
+        let height = height * 2;
+
+        let engine_ptr: FlutterEngine = std::ptr::null_mut();
+        let mut user_data = UserData {
+            terminal: Terminal::new(width, height),
+        };
+
+        let result = FlutterEngineRun(
+            1,
+            &renderer_config,
+            &args.into(),
+            &mut user_data as *mut UserData as *mut std::ffi::c_void,
+            &engine_ptr as *const FlutterEngine as *mut FlutterEngine,
+        );
+
+        assert_eq!(
+            result, FlutterEngineResult_kSuccess,
+            "Engine started successfully"
+        );
+
+        let display = FlutterEngineDisplay {
+            struct_size: std::mem::size_of::<FlutterEngineDisplay>(),
+            display_id: 0,
+            single_display: true,
+            refresh_rate: FPS as f64,
+        };
+
+        assert_eq!(
+            FlutterEngineNotifyDisplayUpdate(
+                engine_ptr,
+                FlutterEngineDisplaysUpdateType_kFlutterEngineDisplaysUpdateTypeStartup,
+                &display as *const FlutterEngineDisplay,
+                1,
+            ),
+            FlutterEngineResult_kSuccess,
+            "notify display update"
+        );
+
+        let event = FlutterWindowMetricsEvent {
+            struct_size: std::mem::size_of::<FlutterWindowMetricsEvent>(),
             width,
             height,
-        }
-    }
-}
+            pixel_ratio: 1.0,
+            left: 0,
+            top: 0,
+            physical_view_inset_top: 0.0,
+            physical_view_inset_right: 0.0,
+            physical_view_inset_bottom: 0.0,
+            physical_view_inset_left: 0.0,
+        };
+        assert_eq!(
+            FlutterEngineSendWindowMetricsEvent(
+                engine_ptr,
+                &event as *const FlutterWindowMetricsEvent,
+            ),
+            FlutterEngineResult_kSuccess,
+            "Window metrics set successfully"
+        );
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct Pixel {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: u8,
-}
+        let engine_start_time = Duration::from_nanos(FlutterEngineGetCurrentTime());
+        // Always offset instants from `engine_start_time` to match the engine time base.
+        let start_instant = Instant::now();
 
-impl Pixel {
-    pub fn zero() -> Self {
-        Pixel {
-            r: 0,
-            g: 0,
-            b: 0,
-            a: 0,
-        }
-    }
-    fn is_lit(&self) -> bool {
-        if self.a == 0 {
-            return false;
-        }
+        loop {
+            match read().unwrap() {
+                crossterm::event::Event::FocusGained => todo!(),
+                crossterm::event::Event::FocusLost => todo!(),
+                crossterm::event::Event::Key(_) => todo!(),
+                crossterm::event::Event::Mouse(MouseEvent {
+                    kind,
+                    column,
+                    row,
+                    modifiers: _,
+                }) => {
+                    // The terminal renderer merges two pixels (top and bottom) into one.
+                    let row = row * 2;
 
-        return self.r != 0 && self.g != 0 && self.b != 0;
-    }
-}
+                    let (phase, buttons) = match kind {
+                        crossterm::event::MouseEventKind::Down(mouse_button) => (
+                            FlutterPointerPhase_kDown,
+                            to_flutter_mouse_button(mouse_button),
+                        ),
+                        crossterm::event::MouseEventKind::Up(mouse_button) => (
+                            FlutterPointerPhase_kUp,
+                            to_flutter_mouse_button(mouse_button),
+                        ),
+                        // Just continue as it's too annoying to log these common events.
+                        crossterm::event::MouseEventKind::Drag(_) => continue,
+                        crossterm::event::MouseEventKind::Moved => continue,
+                        kind => {
+                            println!("ignoring event {kind:?}");
+                            continue;
+                        }
+                    };
 
-impl From<Pixel> for Color {
-    fn from(Pixel { r, g, b, a: _ }: Pixel) -> Self {
-        Color::Rgb { r, g, b }
-    }
-}
+                    let next_time =
+                        Instant::now().duration_since(start_instant) + engine_start_time;
 
-impl Drop for TerminalWindow {
-    fn drop(&mut self) {
-        self.stdout.execute(DisableMouseCapture).unwrap();
-        disable_raw_mode().unwrap();
-        self.stdout.execute(Show).unwrap();
-        self.stdout.execute(LeaveAlternateScreen).unwrap();
-    }
-}
+                    let flutter_pointer_event = FlutterPointerEvent {
+                        struct_size: std::mem::size_of::<FlutterPointerEvent>(),
+                        phase,
+                        timestamp: next_time.as_micros() as usize,
+                        x: column as f64,
+                        y: row as f64,
+                        device: 0,
+                        signal_kind: 0,
+                        scroll_delta_x: 0.0,
+                        scroll_delta_y: 0.0,
+                        device_kind: FlutterPointerDeviceKind_kFlutterPointerDeviceKindMouse,
+                        // This is probably a bitmask for multiple buttons so the
+                        // type doesn't match.
+                        buttons: buttons as i64,
+                        pan_x: 0.0,
+                        pan_y: 0.0,
+                        scale: 0.0,
+                        rotation: 0.0,
+                    };
 
-impl TerminalWindow {
-    pub fn update(&mut self, buffer: &Vec<Pixel>) -> Result<(), Error> {
-        let mut buffer = buffer.to_vec();
-        // Always process an even number of rows.
-        if buffer.len() % 2 != 0 {
-            buffer.extend(vec![Pixel::zero(); self.width]);
-        }
-
-        // Each element is one pixel, but when it is rendered to the terminal,
-        // two rows share one character, using the unicode BLOCK characters.
-
-        // Group alternate rows together, so zipping them allows two consecutive
-        // rows to be processed into terminal characters at the same time.
-        let tops = buffer
-            .iter()
-            .enumerate()
-            .filter(|(i, _)| {
-                let row = i / self.width;
-
-                if row % 2 == 0 {
-                    true
-                } else {
-                    false
+                    FlutterEngineSendPointerEvent(engine_ptr, &flutter_pointer_event, 1);
+                    FlutterEngineScheduleFrame(engine_ptr);
                 }
-            })
-            .map(|(_, val)| *val);
-        let bottoms = buffer
-            .iter()
-            .enumerate()
-            .filter(|(i, _)| {
-                let row = i / self.width;
-
-                if row % 2 == 1 {
-                    true
-                } else {
-                    false
-                }
-            })
-            .map(|(_, val)| *val);
-
-        let lines = zip(tops, bottoms)
-            .enumerate()
-            .fold(vec![], |mut acc, (i, (top, bottom))| {
-                if i % self.width == 0 {
-                    acc.push(vec![]);
-                }
-                let character = match (top.is_lit(), bottom.is_lit()) {
-                    // Just use the top for the color for now.
-                    (true, true) => (top, BLOCK_FULL),
-                    (true, false) => (top, BLOCK_UPPER),
-                    (false, true) => (bottom, BLOCK_LOWER),
-                    (false, false) => (Pixel::zero(), BLOCK_EMPTY),
-                };
-
-                let current_line = acc.last_mut().unwrap();
-                current_line.push(character);
-
-                acc
-            });
-
-        assert_eq!(lines.len(), self.height / 2);
-        assert_eq!(lines.len(), self.lines.len());
-
-        // Refreshing the entire terminal (with the clear char) and outputting
-        // everything on every iteration is costly and causes the terminal to
-        // flicker.
-        //
-        // Instead, only "re-render" the current line, if it is different from
-        // the previous frame.
-
-        for (i, (prev, current)) in zip(&self.lines, &lines).enumerate() {
-            if !do_vecs_match(prev, current) {
-                self.stdout.queue(MoveTo(0, i as u16))?;
-                self.stdout.queue(Clear(ClearType::CurrentLine))?;
-
-                for (pixel, char) in current {
-                    let color: Color = Color::from(*pixel);
-
-                    self.stdout
-                        .queue(PrintStyledContent(char.to_string().with(color)))?;
-                }
+                crossterm::event::Event::Paste(_) => todo!(),
+                crossterm::event::Event::Resize(_, _) => todo!(),
             }
         }
-
-        self.stdout.flush()?;
-
-        self.lines = lines;
-
-        Ok(())
     }
 }
 
-fn do_vecs_match<T: PartialEq>(a: &[T], b: &[T]) -> bool {
-    let matching = a.iter().zip(b.iter()).filter(|&(a, b)| a == b).count();
-    matching == a.len() && matching == b.len()
-}
-
-const BLOCK_LOWER: char = '▄';
-const BLOCK_UPPER: char = '▀';
-const BLOCK_FULL: char = '█';
-const BLOCK_EMPTY: char = ' ';
-
-impl From<ErrorKind> for Error {
-    fn from(value: ErrorKind) -> Self {
-        Error::ErrorStr(value.to_string())
+fn to_flutter_mouse_button(button: MouseButton) -> FlutterPointerMouseButtons {
+    match button {
+        crossterm::event::MouseButton::Left => {
+            FlutterPointerMouseButtons_kFlutterPointerButtonMousePrimary
+        }
+        crossterm::event::MouseButton::Right => {
+            FlutterPointerMouseButtons_kFlutterPointerButtonMouseSecondary
+        }
+        crossterm::event::MouseButton::Middle => {
+            FlutterPointerMouseButtons_kFlutterPointerButtonMouseMiddle
+        }
     }
 }
 
-#[derive(Debug)]
-pub enum Error {
-    ErrorStr(String),
+struct UserData {
+    terminal: Terminal,
 }
