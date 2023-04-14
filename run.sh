9dd6e5498ae8dd877c15923553f79f58cbbcbad4
Jia Hao Goh <jiahaog@users.noreply.github.com>
2023-04-14 22:32:27 +0800

    Add more modes to the build script

--- run.sh
+++ run.sh
@@ -2,10 +2,26 @@
 
 set -euo pipefail
 
+mode="${1:-}"
+
 pushd example
 ../third_party/flutter/bin/flutter build bundle
 popd
 
-cargo run -- \
-  --icu-data-path third_party/flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat \
-  --assets-dir example/build/flutter_assets \
+if [ "$mode" = 'debug' ]; then
+  export LD_LIBRARY_PATH="$(dirname $(find 'target' -name 'libflutter_engine.so'))"
+
+  cargo build
+
+  rust-lldb target/debug/flt -o run -- \
+    --icu-data-path third_party/flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat \
+    --assets-dir example/build/flutter_assets
+elif [ "$mode" = 'asan' ]; then
+  RUSTFLAGS=-Zsanitizer=address cargo run -Zbuild-std --target x86_64-unknown-linux-gnu -- \
+    --icu-data-path third_party/flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat \
+    --assets-dir example/build/flutter_assets
+else
+  cargo run -- \
+    --icu-data-path third_party/flutter/bin/cache/artifacts/engine/linux-x64/icudtl.dat \
+    --assets-dir example/build/flutter_assets 
+fi
