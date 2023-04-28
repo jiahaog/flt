cf7dda471580dfd5f6742ae0fa327f1b784e1277
Jia Hao Goh <jiahaog@users.noreply.github.com>
2023-04-28 20:48:06 +0800

    Fix lint

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
